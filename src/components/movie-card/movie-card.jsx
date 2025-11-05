3c9c3cd1ce13e5c393fe4073a06312f62a7ca014
Sal Estrada <sal.estrada21@gmail.com>
2025-11-05 17:14:08 -0500

    Refactored React

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,6 +1,8 @@
+import React from "react";
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
-import "./movie-card.scss"
+import { Link } from "react-router-dom";
+import "./movie-card.scss";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
@@ -9,12 +11,15 @@ export const MovieCard = ({ movie, onMovieClick }) => {
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text>{movie.director}</Card.Text>
-        <Button onClick={() => onMovieClick(movie)} variant="link">Open</Button>
+        <Link to={`/movies/${encodeURIComponent(movie.id)}`}>
+          <Button onClick={() => onMovieClick(movie)} variant="link">
+            Open
+          </Button>
+        </Link>
       </Card.Body>
     </Card>
   );
 };
-
 
 MovieCard.propTypes = {
   movie: PropTypes.shape({
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from "react-dom/client";
-import { MainView } from "./main-view/main-view";
+import { MainView } from "./components/main-view/main-view";
 import Container from "react-bootstrap/Container";
+import "bootstrap/dist/css/bootstrap.min.css"; 
 import "./index.scss";
 
 const MyFlixApplication = () => {
